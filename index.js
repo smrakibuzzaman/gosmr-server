6fe00c23c869918b829280427f64d8278dd0a82b
smrakibuzzaman <zaman.smr@gmail.com>
2021-11-06 03:42:20 +0600

    second commit

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require('express');
+const app = express();
 const { MongoClient } = require('mongodb');
 const ObjectId = require("mongodb").ObjectId;
 const cors = require('cors')
-require('dotenv').config();
-const app = express();
+require("dotenv").config();
+
 const port = process.env.PORT || 5000;
 
 app.use(cors());
@@ -11,6 +12,7 @@ app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.wl2z9.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
 
 
 async function run() {
@@ -64,13 +66,14 @@ async function run() {
         res.json(result);
             })
    
+            console.log("all route is working");
     } finally {
     //   await client.close();
     }
   }
   run().catch(console.dir);
 
-app.get('/', (req, res) =>{
+app.get("/", (req, res) =>{
     console.log("running GoSMR Server");
 });
 
